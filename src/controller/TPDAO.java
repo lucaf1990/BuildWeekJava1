7c2fbcdc51919850d9578ea30e39e2f32f87d5ad
Vincenzo M <vincenzomaiorana@gmail.com>
2023-04-04 12:22:41 +0200

    save/update/getID/byVend/byDate tp

---
+++ src/controller/TPDAO.java
@@ -0,0 +1,52 @@
+package controller;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import javax.persistence.PersistenceException;
+import javax.persistence.Query;
+
+import model.TravelPasses;
+import model.VendingMachine;
+
+public class TPDAO {
+	
+	public static void saveTicket(TravelPasses tp) {
+		MainProject.em.getTransaction().begin();
+		MainProject.em.persist(tp);
+		MainProject.em.getTransaction().commit();
+		System.out.println("Pass was correctly saved in your Tickets/Passes list");
+	}
+	
+	public static void updateTicket(TravelPasses tp) {
+		MainProject.em.getTransaction().begin();
+		MainProject.em.merge(tp);
+		MainProject.em.getTransaction().commit();
+		System.out.println("Pass/Ticket was correctly updated");
+	}
+	
+	@SuppressWarnings("unchecked")
+	public static void listAllTicketsByDate(LocalDate start, LocalDate end) {
+		Query q = MainProject.em.createNamedQuery("TravelPasses.listAllTicketsByDate");
+		q.setParameter("start", start);
+		q.setParameter("end", end);
+		List<TravelPasses> list = q.getResultList();
+		System.out.println(list.size() + " tickets was printed between " + start.toString() + " and " + end.toString());
+	}
+	
+	@SuppressWarnings("unchecked")
+	public static void listAllTicketsByVendor(VendingMachine vm) {
+		Query q = MainProject.em.createNamedQuery("TravelPasses.listAllTicketsByVendor");
+		q.setParameter("vm", vm);
+		List<TravelPasses> list = q.getResultList();
+		System.out.println(list.size() + " tickets was printed from " + vm.toString());
+	}
+	
+	public static TravelPasses getByID(Long id) throws PersistenceException {
+		MainProject.em.getTransaction().begin();
+		TravelPasses tp = MainProject.em.find(TravelPasses.class, id);
+		MainProject.em.getTransaction().commit();
+		return tp;
+	}
+
+}
